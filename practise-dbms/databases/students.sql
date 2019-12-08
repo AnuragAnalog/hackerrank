a3b1b821e0de2fef0d278bb1eb9cde7b6b70ed99
AnuragAnalog <anurag.peddi1998@gmail.com>
2019-12-08 19:34:41 +0530

    added students database

---
+++ practise-dbms/databases/students.sql
@@ -0,0 +1,35 @@
+create database if not exists hackerrank;
+use hackerrank;
+
+create table if not exists students
+(
+    id int,
+    name varchar(20),
+    marks int
+);
+
+insert into students values (19,"Samantha",87);
+insert into students values (21,"Julia",96);
+insert into students values (11,"Britney",95);
+insert into students values (32,"Kristeen",100);
+insert into students values (12,"Dyana",55);
+insert into students values (13,"Jenny",66);
+insert into students values (14,"Christene",88);
+insert into students values (15,"Meera",24);
+insert into students values (16,"Priya",76);
+insert into students values (17,"Priyanka",77);
+insert into students values (18,"Paige",74);
+insert into students values (19,"Jane",64);
+insert into students values (21,"Belvet",78);
+insert into students values (31,"Scarlet",80);
+insert into students values (41,"Salma",81);
+insert into students values (51,"Amanda",34);
+insert into students values (61,"Heraldo",94);
+insert into students values (71,"Stuart",99);
+insert into students values (81,"Aamina",77);
+insert into students values (76,"Amina",89);
+insert into students values (91,"Vivek",84);
+insert into students values (17,"Evil",79);
+insert into students values (16,"Devil",76);
+insert into students values (34,"Fanny",75);
+insert into students values (38,"Danny",75);
