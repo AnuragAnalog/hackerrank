cf0bfd41786b4102d2e6e94e68bdacfb2b76fced
Dmitriy Alekseiev <47810339+dimonalek@users.noreply.github.com>
2021-10-13 12:27:19 +0300

    Update AreaOfCircle.py
    Refactored. Catched possible wrong inputs

--- practise-python/math/AreaOfCircle.py
+++ practise-python/math/AreaOfCircle.py
@@ -1,19 +1,22 @@
 #! bin/python3
-
 import math
-
-mode = input("What will we use to count area of circle? Length of circumference[l], Radius[r] or Diameter[d]?: ")
-if "d" in mode or "D" in mode:
-    d = input("What diameter of circle: ")
-    s = (math.pi*math.pow(int(d),2))/4
-    print("Area of circle is: ", s)
-elif "r" in mode or "R" in mode:
-    r = input("What radius of circle: ")
-    s = math.pi*(math.pow(int(r),2))
-    print("Area of circle is:", s)
-elif "l" in mode or "L" in mode:
-    l = input("What length of circumference: ")
-    s = math.pow(int(l),2)/(math.pi*4)
-    print("Area of circle is:", s)
-else:
-    print("Wrong input. Try again")
+i = 1
+while i == 1:
+    mode = input("What will we use to count area of circle? Length of circumference[l], Radius[r] or Diameter[d]?: ")
+    if mode == "d" or mode == "D":
+        d = input("What diameter of circle: ")
+        s = (math.pi*math.pow(int(d),2))/4
+        print("Area of circle is: ", s)
+        break
+    elif mode == "r" or mode == "R":
+        r = input("What radius of circle: ")
+        s = math.pi*(math.pow(int(r),2))
+        print("Area of circle is:", s)
+        break
+    elif mode == "l" or mode == "L":
+        l = input("What length of circumference: ")
+        s = math.pow(int(l),2)/(math.pi*4)
+        print("Area of circle is:", s)
+        break
+    else:
+        print("Wrong input. Try again")
